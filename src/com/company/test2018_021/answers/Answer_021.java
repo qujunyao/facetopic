1c696f066527ee3e7cbda370b66175939648fb31
休闲杀人 <38315789+qujunyao@users.noreply.github.com>
2019-02-13 14:18:43 +0800

    提交记录

--- src/com/company/test2018_021/answers/Answer_021.java
+++ src/com/company/test2018_021/answers/Answer_021.java
@@ -1,20 +1,18 @@
-package com.company.test2018_001.questions;
+package com.company.test2018_021.answers;
 
 import java.util.*;
 
-/*
-	
- */
-public class Question_021 {
+public class Answer_021 {
+	@SuppressWarnings("unused")
 	public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         for (int i = 0; i < n-1; i++) {
             int x = sc.nextInt();
-            x = sc.nextInt();
-            if(x==0) ;         
+            int y = sc.nextInt();
         }
         sc.close();
         System.out.println((int)Math.pow(2, n-1));
     }
 }
+
